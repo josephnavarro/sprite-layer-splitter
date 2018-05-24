fefd6c56ef557af3566c823d9dd71336624e7e09
Joseph Immanuel Navarro <navarro0@purdue.edu>
2018-05-23 20:12:45 -0400

    Fixed dark falcon female sprite

--- splitter.py
+++ splitter.py
@@ -104,7 +104,12 @@ HEAD_PARAMS = {
         'reverse': True,
         },
     'dark-falcon-f': {
-        'offset': [],
+        'offset': [(1,0),(2,0),(2,-1),(1,-1),],
+        'size': 'small',
+        },
+    'dark-falcon-m': {
+        'offset': [(0,0),(0,0),(0,0),(0,0),],
+        'size': 'small',
         },
     }
 
--- test.py
+++ test.py
@@ -6,8 +6,8 @@ import splitter, glob, os
 # (https://www.spriters-resource.com/3ds/fireemblemfates/)
 
 FILE_STRING = 'inputs/{}.png'
-UNIT_NAME = 'peri'
-CLASS_NAME = 'basara-f'
+UNIT_NAME = 'azura'
+CLASS_NAME = 'dark-falcon-f'
 
 splitter.main(
     FILE_STRING.format(UNIT_NAME),
