81d41b61a1a76fb9f37e7b323e837fb95c034302
Joseph Immanuel Navarro <navarro0@purdue.edu>
2018-05-24 02:17:05 -0400

    Fixed fighter male sprite

--- splitter.py
+++ splitter.py
@@ -147,6 +147,10 @@ HEAD_PARAMS = {
         'size': 'small',
         #'reverse': True,
         },
+    'fighter-m': {
+        'offset': [(1,0),(1,1),(1,2),(1,1),],
+        'size': 'large',
+        },
     'wyvern-lord': {
         'offset': [(-2,0),(-2,-2),(-2,-1),(-2,-1),],
         'size': 'small',
