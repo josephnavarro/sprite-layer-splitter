7bf8f90a3c9d9602addd2fdc912a0f104eea8762
Joseph Immanuel Navarro <navarro0@purdue.edu>
2018-05-26 18:58:09 -0400

    Fixed samurai-m

--- splitter.py
+++ splitter.py
@@ -334,6 +334,10 @@ HEAD_PARAMS = {
         'offset': [(0,3),(0,3),(0,3),(0,3),],
         'size': 'large',
         },
+    'samurai-m': {
+        'offset': [(0,0),(0,0),(0,-1),(0,-2),],
+        'size': 'large',
+        },
     'wyvern-lord': {
         'offset': [(-2,0),(-2,-2),(-2,-1),(-2,-1),],
         'size': 'small',
